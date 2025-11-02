dc8a18ad7a2ce419dfb58465059d2e6821a42df5
Lois <lois@mejias.com>
2025-11-02 15:59:49 -0400

    feat: Cambio a false  en el envio de correo

--- src/app/components/form-dose/form-dose.component.ts
+++ src/app/components/form-dose/form-dose.component.ts
@@ -97,7 +97,7 @@ export class FormDoseComponent implements OnInit {
         this.showModal.set(true);
 
         // MANEJO DE ESTADO DE ENVÍO DE CORREO (Ahora que la bandera es obligatoria si hay email)
-        if (dataToSend.sendEmail) {
+        if (dataToSend.sendEmail === true) {
           if (response.emailSent) {
             this.emailStatusMessage =
               '¡El análisis fue enviado a su correo electrónico!';
