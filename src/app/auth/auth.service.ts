cd763a0e959404838266c9e23cfa453166976957
Lois <lois@mejias.com>
2025-11-10 19:38:05 -0400

    Cambio a URL directa de la respuesta a Whatsapp

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,8 +3,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import { environment } from '../../env/environment.production';
-import { environment } from '../../env/environment.development';
+import { environment } from '../../env/environment.production';
 
 
 @Injectable({
